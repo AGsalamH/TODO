ac629bdababd351d18993f2d69ecb3be91f73986
Ahmed Gamal <agsalamh@gmail.com>
2021-01-29 06:52:37 +0200

    change required field error message

--- models/todo.js
+++ models/todo.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+// const contextService = require('request-context');
 
 const todoSchema = new Schema({
     todo: {
         type: String,
-        required: true,
+        required: [true, 'Can\'t be empty'],
     },
 
     done: {
