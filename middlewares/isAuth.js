71b41fa227f3571c21704d5efba663f77884f017
Ahmed Gamal <agsalamh@gmail.com>
2021-02-04 05:13:46 +0200

    Fix: `isAuth` middleware To check if user exists even if token is valid

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
-const {jwtError, _throw} = require('../utils/errorHandling');
+const {jwtError,isMongooseError, _throw} = require('../utils/errorHandling');
+const User = require('../models/user');
 
 // Protect routes and make sure user is logged in
 // Authorization
-module.exports = (req, res, next) =>{
+module.exports = async (req, res, next) =>{
     const token = req.get('auth-token');
     try {
         if(!token){
@@ -12,9 +13,15 @@ module.exports = (req, res, next) =>{
             throw error;
         }
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        const user = await User.findById(verified._id);
+        if(!user){
+            const error = new jwt.JsonWebTokenError('User not found!');
+            error.statusCode = 404;
+            throw error;
+        }
         req.user = verified;
         next();
-    } catch (error) {
-        jwtError(error) ? next(error) : _throw(error);
+    } catch (err) {
+        jwtError(err) || isMongooseError(err) ? next(err) : _throw(err);
     }  
 }
